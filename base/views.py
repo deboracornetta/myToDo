ed4b63df15c4bca9b1437cd6e1f6f130f6443c99
Debora Cornetta <cornettaintech@gmail.com>
2022-04-23 20:52:18 +0100

    CRUD functionalities finalized

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from re import template
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -23,4 +23,15 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
